d5b8f314ad26aa22168c4b42989391a8667fc69e
sknaht <yangzy.orz@gmail.com>
2016-04-14 20:43:57 -0700

    adapter more

--- app/src/main/java/org/me/tuoristguide/ui/adapter/BaseListAdapter.java
+++ app/src/main/java/org/me/tuoristguide/ui/adapter/BaseListAdapter.java
@@ -1,31 +1,90 @@
 package org.me.tuoristguide.ui.adapter;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by zy on 4/14/16.
  */
-public class BaseListAdapter extends BaseAdapter {
+public abstract class BaseListAdapter<T, V extends View> extends RecyclerView.Adapter<BaseListAdapter.ViewHolder<V>> {
+
+    private List<T> list = Collections.emptyList();
+    protected Context context;
+
+    public BaseListAdapter(Context context) {
+        this.context = context;
+    }
+
+    protected abstract V createView(Context context, ViewGroup viewGroup, int viewType);
+
+    protected abstract void bind(T value, V view, ViewHolder<V> holder);
 
     @Override
-    public int getCount() {
-        return 0;
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder<V>(createView(context, parent, viewType));
     }
 
     @Override
-    public Object getItem(int position) {
-        return null;
+    public void onBindViewHolder(ViewHolder<V> holder, int position) {
+        bind(list.get(position), holder.view, holder);
     }
 
     @Override
-    public long getItemId(int position) {
-        return 0;
+    public int getItemCount() {
+        return list.size();
     }
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+    public void setItems(List<T> list) {
+        this.list = list;
+        notifyDataSetChanged();
     }
+
+    public List<T> getItems() {
+        return list;
+    }
+
+    public void addItem(T item) {
+        list.add(item);
+        notifyDataSetChanged();
+    }
+
+    public void addFirst(T item) {
+        list.add(0, item);
+        notifyItemInserted(0);
+    }
+
+    public void clearItems() {
+        list.clear();
+        notifyDataSetChanged();
+    }
+
+    public T getItem( int position){
+        return list.get(position);
+    }
+
+
+    public T getFirstItem(){
+        return list.get(0);
+    }
+
+    public void removeItem(int position) {
+        list.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public static class ViewHolder<V extends View> extends RecyclerView.ViewHolder {
+        V view;
+
+        public ViewHolder(V view) {
+            super(view);
+            this.view = view;
+        }
+    }
+
 }
