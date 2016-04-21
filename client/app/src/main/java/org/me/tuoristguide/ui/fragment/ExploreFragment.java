2996ff7cacc1e4b6695d79c787e32a505cc34d37
sknaht <yangzy.orz@gmail.com>
2016-04-21 16:41:17 -0700

    separate yelpservice

--- client/app/src/main/java/org/me/tuoristguide/ui/fragment/ExploreFragment.java
+++ client/app/src/main/java/org/me/tuoristguide/ui/fragment/ExploreFragment.java
@@ -1,7 +1,5 @@
 package org.me.tuoristguide.ui.fragment;
 
-
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -16,53 +14,45 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.yelp.clientlib.connection.YelpAPI;
-import com.yelp.clientlib.connection.YelpAPIFactory;
 import com.yelp.clientlib.entities.Business;
-import com.yelp.clientlib.entities.SearchResponse;
-import com.yelp.clientlib.entities.options.BoundingBoxOptions;
 
 import org.me.tuoristguide.R;
+import org.me.tuoristguide.service.local.YelpService;
 import org.me.tuoristguide.ui.activity.DetailActivity;
 import org.me.tuoristguide.ui.activity.MainActivity;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
-import retrofit.Call;
-import retrofit.Callback;
-import retrofit.Response;
-import retrofit.Retrofit;
 
 
-public class ExploreFragment extends Fragment {
+public class ExploreFragment extends Fragment implements YelpService.YelpServiceInterface {
 
     private static View view;
     private static Double latitude, longitude;
     private static GoogleMap mMap;
 
-    Call<SearchResponse> call;
-
 
     public ExploreFragment() {
-
+        // set self as controller in YelpService
+        YelpService.getInstance().setController(this);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
                              Bundle savedInstanceState) {
 
         if (container == null) {
             return null;
         }
 
-           view = (RelativeLayout) inflater.inflate(R.layout.fragment_explore, container, false);
+        view = (RelativeLayout) inflater.inflate(R.layout.fragment_explore, container, false);
 
+        // TODO change to getting real
         latitude = 37.422006;
         longitude = -122.084095;
         setUpMapIfNeeded();
-        yelpNearby(latitude, longitude);
+        YelpService.getInstance().yelpNearby(latitude, longitude);
+
         return view;
     }
 
@@ -77,7 +67,8 @@ public class ExploreFragment extends Fragment {
         }
     }
 
-    private static void setUpMap(ArrayList<Business> businesses) {
+    // interface method from YelpService interface
+    public void setUpMap(ArrayList<Business> businesses) {
         // For showing a move to my loction button
         //mMap.setMyLocationEnabled(true);
         // For dropping a marker at a point on the Map
@@ -94,21 +85,6 @@ public class ExploreFragment extends Fragment {
         mMap.getUiSettings().setZoomGesturesEnabled(true);
     }
 
-//    @Override
-//    public void onViewCreated(View view, Bundle savedInstanceState) {
-//        // TODO Auto-generated method stub
-//        if (mMap != null)
-//            setUpMap();
-//
-//        if (mMap == null) {
-//            // Try to obtain the map from the SupportMapFragment.
-//            mMap = ((MapFragment) MainActivity.fragmentManager
-//                    .findFragmentById(R.id.explore_map)).getMap(); // getMap is deprecated
-//            // Check if we were successful in obtaining the map.
-//            if (mMap != null)
-//                setUpMap();
-//        }
-//    }
 
     @Override
     public void onDestroyView() {
@@ -118,49 +94,6 @@ public class ExploreFragment extends Fragment {
                     .remove(MainActivity.fragmentManager.findFragmentById(R.id.explore_map)).commit();
             mMap = null;
         }
-        call.cancel();
-    }
-
-    /***
-     * This method is used to find all nearby recommendations
-     *
-     * @param latitude
-     * @param longitude
-     * @return
-     */
-    public void yelpNearby(Double latitude, Double longitude) {
-        YelpAPIFactory apiFactory = new YelpAPIFactory("NUBhGlHwegeyRXi5Ci9XLA", "evZpCq1B7GcjEv-5WgFYH2iNBcM",
-                "Rl5mlNSOFd4sJ9G4KNVjDWa7wLmdyUVG", "31cVuZroz3LPGrfRqy6IAfpn3n0");
-        YelpAPI yelpAPI = apiFactory.createAPI();
-        Map<String, String> queryParams = new HashMap<String, String>();
-        queryParams.put("limit", "20");
-        BoundingBoxOptions bouds = BoundingBoxOptions.builder()
-                .swLatitude(latitude - 0.01)
-                .swLongitude(longitude - 0.01)
-                .neLatitude(latitude + 0.01)
-                .neLongitude(longitude + 0.01).build();
-
-
-
-        Callback<SearchResponse> callback = new Callback<SearchResponse>() {
-            @Override
-            public void onResponse(Response<SearchResponse> response, Retrofit retrofit) {
-
-                System.out.println("here is the response!!! " + response.body().businesses());
-                ArrayList<Business> businesses=response.body().businesses();
-                if (mMap != null)
-                    setUpMap(businesses);
-            }
-
-            @Override
-            public void onFailure(Throwable t) {
-                System.out.println("something wrong!!");
-                System.out.println(t);
-            }
-        };
-        call = yelpAPI.search(bouds, queryParams);
-        call.enqueue(callback);
-
     }
 
 
