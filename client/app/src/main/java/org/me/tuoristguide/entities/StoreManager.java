b3452b5c6f891fe6296ec6995923d888ee6a47f7
caoyielliot <caoyiee@outlook.com>
2016-04-24 10:21:22 -0700

    detail

---
+++ client/app/src/main/java/org/me/tuoristguide/entities/StoreManager.java
@@ -0,0 +1,7 @@
+package org.me.tuoristguide.entities;
+
+/**
+ * Created by caoyi on 16/4/23.
+ */
+public class StoreManager {
+}
---
+++ client/app/src/main/java/org/me/tuoristguide/service/remote/CommentService.java
@@ -0,0 +1,7 @@
+package org.me.tuoristguide.service.remote;
+
+/**
+ * Created by caoyi on 16/4/24.
+ */
+public class CommentService {
+}
